00c91a95937588c207a1217e55dc0bd9fcdc692c
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2022-02-05 16:44:47 +0330

    feat(app): Change timezone, DB_Config

--- E_Bank/E_Bank/settings.py
+++ E_Bank/E_Bank/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'E_Bank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'E_Bank',
+        'USER': 'postgres',
+        'PASSWORD': 's1234567',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
