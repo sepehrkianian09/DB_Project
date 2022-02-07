1ffcf0ac69ba0021a1c4b0cd59ee795f1477e53b
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2022-02-07 16:06:47 +0330

    feat(user): Add validation on employeeExtraInfo

--- user/models/employee/employeeExtraInfo.py
+++ user/models/employee/employeeExtraInfo.py
@@ -1,3 +1,6 @@
+import datetime
+
+from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
@@ -10,18 +13,32 @@ class EmployeeWorkingHours(models.Model):
         MinValueValidator(0),
         MaxValueValidator(6),
     ])
-    start_hour = models.TimeField()
-    end_hour = models.TimeField()
+    start_hour = models.TimeField(default=datetime.time())
+    end_hour = models.TimeField(default=datetime.time())
+
+    def clean(self):
+        super().clean()
+        if self.end_hour <= self.start_hour:
+            raise ValidationError(f"start_hour should be smaller than end_hour")
 
     class Meta:
         unique_together = ['employee_id', 'working_day']
 
 
+def validate_employee_on_change_account(employee_id):
+    the_employee = Employee.objects.get(pk=employee_id)
+    if not (the_employee.status and the_employee.can_block_account):
+        raise ValidationError(f"the employee must be on work and be able to block account")
+
+
 class EmployeeChangeAccountTransaction(models.Model):
-    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE, primary_key=True)
+    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE, validators=[
+        validate_employee_on_change_account
+    ])
     # account_id = models.ForeignKey(employee, on_delete=models.CASCADE)
-    new_state = models.BooleanField
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    # constraint: Account.state = count(ChangeAccountTransaction) % 2 == 0
 
     # class Meta:
     # unique_together = ['employee_id', 'account_id', 'date']
