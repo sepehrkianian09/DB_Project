92ee65ecf0c2e0b1a1123343310364f00dc66ecf
haxtap <heliarosha@yahoo.com>
2022-02-08 05:36:54 +0330

    view for human and customer except for accour

--- E_Bank/settings.py
+++ E_Bank/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'dbview'
     'django.contrib.staticfiles',
     'user.apps.UserConfig',
     'account.apps.AccountConfig',
