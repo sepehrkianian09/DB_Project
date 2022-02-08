ffe30fa72b5a0a86bb8738cbbbfa1920b14b9479
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2022-02-09 01:01:00 +0330

    feat(financial): Balance Computation and integrity in transactions

--- financial/models/transaction.py
+++ financial/models/transaction.py
@@ -2,13 +2,16 @@ from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from django.db import models
 
-from account.models import Account
+from account.models import Account, CustomerAccount
+from .depoWithTransactionLicense import DepoWithTransactionLicense
 
 
 class Transaction(models.Model):
     transaction_id = models.BigAutoField(primary_key=True)
     amount = models.IntegerField(validators=[MinValueValidator(0)])
     date = models.DateField(auto_now_add=True, editable=False)
+
+    applied = models.BooleanField(default=False)
 
     class Meta:
         abstract = True
@@ -20,9 +23,27 @@ class CardToCardTransaction(Transaction):
 
     def clean(self):
         super().clean()
-        if self.src != self.dst:
+        if self.src == self.dst:
             raise ValidationError("src, dst shouldn't be equal")
+
+        if not self.applied:
+            src_account = Account.objects.get(pk=self.src)
+            if src_account.balance - self.amount < 0:
+                raise ValidationError("src doesn't have enough money")
+            src_account.balance -= self.amount
+            src_account.save()
+            self.applied = True
 
 
 class DepositWithDrawTransaction(Transaction):
-    pass
+    def clean(self):
+        super().clean()
+        if not self.applied:
+            license = DepoWithTransactionLicense.objects.get(depo_with_transaction_id=self.transaction_id)
+            src_account = CustomerAccount.objects.get(pk=license.customer_acc_id)
+
+            if src_account.balance - self.amount < 0:
+                raise ValidationError("src doesn't have enough money")
+            src_account.balance -= self.amount
+            src_account.save()
+            self.applied = True
